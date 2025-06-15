39a00d231f5b59d32cced5a343f4b9897e68cd05
HungTran181100 <165378520+HungTran181100@users.noreply.github.com>
2025-06-15 11:40:07 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,31 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 import streamlit as st
 
-st.write("Đã cập nhật phiên bản mới nhất!")
+load_dotenv()
+google_api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=google_api_key)
+
+model = genai.GenerativeModel(
+    "gemini-2.0-flash-lite",
+    system_instruction=""" 
+                Bạn là một giáo viên dạy lập trình ngôn ngữ python. 
+                1. Hãy hướng dẫn học sinh giải các bài tập liên quan đến lập trình thật chi tiết.
+                2. Hỗ trợ những câu hỏi lý thuyết.
+                3. Không hỗ trợ những chức năng không liên quan đến lập trình
+
+""",
+)
+
+input_text = st.text_input("Nhập nội dung của bạn tại đây", key="input")
+submit = st.button("Gửi")
+
+response = None
+if input_text and submit:
+    response = model.generate_content(input_text)
+
+mess = st.empty()
+
+if response:
+    mess.markdown(response.text)
